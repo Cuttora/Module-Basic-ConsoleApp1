214090651a7af3a1c77ff5d700da35ed9725995a
Cuttora <leogon4arenko@yandex.ru>
2025-08-22 21:48:16 +0700

    Working with a file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Leonid";
+            const string MyName = "Leonid";
             Console.WriteLine(MyName);
+            
+
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 27 лет");
+            Console.WriteLine("\tMy name is \n Leonid");
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+
             Console.ReadKey();
         }
     }
