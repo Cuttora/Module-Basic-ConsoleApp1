0890a685d8541bca42e552c7d4db84a6317d9832
Cuttora <leogon4arenko@yandex.ru>
2025-08-22 20:37:09 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Leonid";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Какое у Вас хобби?");
+            string hobby = Console.ReadLine();
+
+            Console.WriteLine($"\n\nЗдравствуйте, {name}. Благодаря Вашим ответам мы узнали о Вас много нового!)" + "\n- - - - - - - -" + "\n Возраст: " + age
+                + "\n Увлечение: {0}" + "\n- - - - - - - -", hobby);
+
+        }
+
+    }
+}
