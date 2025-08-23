5fff48c2b17a614b0a21cd461243544fbc7cdc65
Cuttora <leogon4arenko@yandex.ru>
2025-08-23 10:29:35 +0700

    Add new file - "DaysOfWeek"

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DaysOfWeek
+{
+    internal class Program
+    {
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
