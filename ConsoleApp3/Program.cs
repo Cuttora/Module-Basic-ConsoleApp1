712671bc7aeebfcc4fa9edbf091ea4b5490854b0
Cuttora <leogon4arenko@yandex.ru>
2025-08-26 20:27:30 +0700

    Questionnaire about you

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp3
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.Write("How old are you? ");
+            var age = checked (byte.Parse(Console.ReadLine()));
+            Console.Write("What is your date of birth (For example, 31.01.2001)? ");
+            string dateOfBirth = Console.ReadLine();
+            Console.WriteLine("Your name is {0} and age is {1}. And you were born on {2} ", name, age, dateOfBirth);
+
         }
     }
 }
