19e36e0a21cc594e30e19aade9027eb02181714d
Cuttora <leogon4arenko@yandex.ru>
2025-09-19 22:09:46 +0700

    add new files => FavDishes and practice(Module 5.1)

---
+++ FavDishes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace FavDishes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string[] FavDishes) User;
+            User.FavDishes = new string [5];
+            Console.WriteLine("What is your name? ");
+            User.name = Console.ReadLine();
+            Console.WriteLine("What is your favorite dishes(TOP 5): ");
+            for (int i = 0; i < User.FavDishes.Length; i++)
+            {
+                Console.WriteLine("TOP {0}" , i + 1);
+                User.FavDishes[i] = Console.ReadLine();
+            }
+            
+        }
+
+    }
+}
---
+++ practice/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace practice
+{
+    internal class Program
+    {
+        // 5.1.5
+        /*static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            string color = Console.ReadLine();
+
+            
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+            Console.WriteLine("Ваши любимые цветы: ");
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                Console.WriteLine(favcolors[i]);
+            }
+            
+        }*/
+        //--------------------------------------------------------------------------------------
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int check;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int t = 0; t < result.Length; t++)
+                {
+                    if (result[i] < result[t])
+                    {
+                        check = result[i];
+                        result[i] = result[t];
+                        result[t] = check;
+                    }
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+           int[] array = GetArrayFromConsole();
+        }
+    }
+
+}
+
