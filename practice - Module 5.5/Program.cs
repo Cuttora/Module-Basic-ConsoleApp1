bb4ef28d2930faaecb246760338b892edb9d2376
Cuttora <leogon4arenko@yandex.ru>
2025-09-20 21:41:10 +0700

    add file practice - module 5.5 (encoded the echo)

---
+++ practice - Module 5.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace practice___Module_5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+        }
+        static void Echo(string phrase, int deep) 
+        {
+            var modif = phrase;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+    }
+}
