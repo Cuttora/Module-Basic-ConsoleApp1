8c38dbe0005ed83bf908c7f196f30f9f2e003ce9
Cuttora <leogon4arenko@yandex.ru>
2025-09-20 21:17:38 +0700

    add new file => practice - Module 5.3

---
+++ practice - Module 5.3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace practice___Module_5._3
+{
+    internal class Program
+    {
+        class MainClass
+        {
+            static void SortArray(int[] array, out int[] sorteddesc, out int[] sortedasc)
+            {
+                sorteddesc = SortArrayDesc(array);
+                sortedasc = SortArrayAsc(array);
+            }
+            static int[] SortArrayDesc(int[] array)
+            {
+                int check = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int t = 0; t < array.Length; t++)
+                    {
+                        if (array[i] > array[t])
+                        {
+                            check = array[i];
+                            array[i] = array[t];
+                            array[t] = check;
+
+                        }
+                    }
+
+                }
+
+                return array;
+            }
+            static int[] SortArrayAsc(int[] array)
+            {
+                int check = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int t = 0; t < array.Length; t++)
+                    {
+                        if (array[i] < array[t])
+                        {
+                            check = array[i];
+                            array[i] = array[t];
+                            array[t] = check;
+
+                        }
+                    }
+
+                }
+
+                return array;
+            }
+            public static void Main(string[] args)
+            {
+
+            }
+
+
+
+        }
+    }
+}
