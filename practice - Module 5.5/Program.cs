3acb589f2654d77ce6ebd9fbec0cee7775bd20a5
Cuttora <leogon4arenko@yandex.ru>
2025-09-22 19:53:49 +0700

    add file => Final work for module 5(Homework)

--- practice - Module 5.5/Program.cs
+++ practice - Module 5.5/Program.cs
@@ -7,13 +7,24 @@ namespace practice___Module_5._5
         static void Main(string[] args)
         {
             Console.WriteLine("Напишите что-то");
-            var str = Console.ReadLine();
+             var str = Console.ReadLine();
 
-            Console.WriteLine("Укажите глубину эха");
-            var deep = int.Parse(Console.ReadLine());
+             Console.WriteLine("Укажите глубину эха");
+             var deep = int.Parse(Console.ReadLine());
 
-            Echo(str, deep);
+             Echo(str, deep);
 
+        }
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
         }
         static void Echo(string phrase, int deep) 
         {
@@ -22,7 +33,9 @@ namespace practice___Module_5._5
             {
                 modif = modif.Remove(0, 2);
             }
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." + modif);
+
 
             if (deep > 1)
             {
